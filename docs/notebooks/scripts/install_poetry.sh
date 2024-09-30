efe4cfffffe5c4e735cb14d494a467d574951931
danielazamoragoogle <danielazamora@google.com>
2024-09-30 05:50:55 +0000

    Deleted extra files (to add them as we test them)

--- docs/notebooks/scripts/install_poetry.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# install poetry
-curl -sSL https://install.python-poetry.org | POETRY_HOME=$HOME/.poetry python3 -
-# add poetry to PATH
-echo "export PATH=$HOME/.poetry/bin:$PATH" >> $HOME/.bash_profile
-source $HOME/.bash_profile
-poetry --version
